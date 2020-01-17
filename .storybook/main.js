4a96ec4939dbcb6d536c2298e4c3e3a5b36e72ed
Chris Wong <chris.wong@forenodew.com>
2020-01-17 21:43:20 +0800

    Install less pre-processors and apply to main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
+const path = require('path');
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+	addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+	webpackFinal: async (config, { configType }) => {
+		config.module.rules.push({
+			test: /\.less$/,
+			use: ['style-loader', 'css-loader', 'less-loader'],
+			include: path.resolve(__dirname, '../src/components'),
+		});
+		return config;
+	},
 };
